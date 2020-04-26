48aabdd172c2242fd9dbfa179e68a152df18a55c
anupama <anupama_avula@yahoo.com>
2020-04-26 19:07:39 -0400

    Updates

---
+++ precache-manifest.3cf19b0eee9939e3fa868c62d970928b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d94c124b1ca78fd1fe4bd6d71057f56",
+    "url": "/react-react_weatherapp/index.html"
+  },
+  {
+    "revision": "2c3d130e2eefd437f87e",
+    "url": "/react-react_weatherapp/static/css/main.8b4e8525.chunk.css"
+  },
+  {
+    "revision": "2567db6cd9016f4298ee",
+    "url": "/react-react_weatherapp/static/js/2.6e731fc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-react_weatherapp/static/js/2.6e731fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3d130e2eefd437f87e",
+    "url": "/react-react_weatherapp/static/js/main.4bbfb05f.chunk.js"
+  },
+  {
+    "revision": "0f489f0874649318f845",
+    "url": "/react-react_weatherapp/static/js/runtime-main.4d3506d7.js"
+  },
+  {
+    "revision": "e6d6aeb8bfffd8c394c6047c42f2db36",
+    "url": "/react-react_weatherapp/static/media/bg1.e6d6aeb8.png"
+  }
+]);
